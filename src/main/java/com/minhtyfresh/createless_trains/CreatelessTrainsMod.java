cd03f1beab77c54ec87e61afb89eee91ce1cbcb5
minhty-fresh <4746495+minhty-fresh@users.noreply.github.com>
2025-05-14 17:04:41 -0700

    load built in resource pack

--- src/main/java/com/minhtyfresh/createless_trains/CreatelessTrainsMod.java
+++ src/main/java/com/minhtyfresh/createless_trains/CreatelessTrainsMod.java
@@ -5,13 +5,16 @@ import com.simibubi.create.Create;
 import io.github.fabricators_of_create.porting_lib.util.EnvExecutor;
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.fabricmc.fabric.api.resource.ResourcePackActivationType;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.resources.ResourceLocation;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class CreatelessTrainsMod implements ModInitializer {
-	public static final String ID = "createless-trains";
+	public static final String MOD_ID = "createless_trains";
 	public static final String NAME = "Create-less Trains";
 	public static final Logger LOGGER = LoggerFactory.getLogger(NAME);
 
@@ -22,9 +25,12 @@ public class CreatelessTrainsMod implements ModInitializer {
 				() -> () -> "{} is accessing Porting Lib from the client!",
 				() -> () -> "{} is accessing Porting Lib from the server!"
 		), NAME);
+
+		FabricLoader.getInstance().getModContainer(MOD_ID).ifPresent(modContainer -> ResourceManagerHelper.registerBuiltinResourcePack(CreatelessTrainsMod.id("createless_trains"), modContainer, ResourcePackActivationType.ALWAYS_ENABLED));
+
 	}
 
 	public static ResourceLocation id(String path) {
-		return new ResourceLocation(ID, path);
+		return new ResourceLocation(MOD_ID, path);
 	}
 }
